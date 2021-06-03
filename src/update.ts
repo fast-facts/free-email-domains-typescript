1bb1dd3b09741f92a9d789bcd11275493d293636
RabbitPlayground <107660271+RabbitPlayground@users.noreply.github.com>
2021-06-03 17:15:32 -0400

    bug fix

--- src/update.ts
+++ src/update.ts
@@ -5,22 +5,22 @@ import * as hash from 'object-hash';
 // tslint:disable-next-line: no-var-requires
 const cmd = require('node-run-cmd');
 
-const fileLocation = 'dist/domains.json';
+const fileLocation = 'src/index.ts';
 
 // https://knowledge.hubspot.com/forms/what-domains-are-blocked-when-using-the-forms-email-domains-to-block-feature
 axios.get('https://f.hubspotusercontent40.net/hubfs/2832391/Marketing/Lead-Capture/free-domains-1.csv')
   .then(response => {
-    const data = response.data.split(/,\n/g);
+    const data = 'export const domains = ' + JSON.stringify(response.data.split(/,\n/g));
 
     if (data.length < 100) {
       throw new Error('Domain count too low');
     }
 
-    const curHash = hash(JSON.parse(fs.readFileSync(fileLocation).toString()));
+    const curHash = hash(fs.readFileSync(fileLocation).toString());
     const newHash = hash(data);
 
     if (curHash !== newHash) {
-      fs.writeFileSync('dist/domains.json', JSON.stringify(data));
+      fs.writeFileSync(fileLocation, data);
       cmd.run('cd dist && npm version --no-git-tag-version patch', { shell: true });
     }
   })
