473d7ef7680303a5258907dbe35a949a82ba0d23
RabbitPlayground <107660271+RabbitPlayground@users.noreply.github.com>
2022-06-11 10:35:18 -0400

    Fix: Puppeteer in update script

---
+++ src/chrome.ts
@@ -0,0 +1,86 @@
+import * as  Puppeteer from 'puppeteer';
+import * as  _puppeteer from 'puppeteer-pro';
+
+_puppeteer.avoidDetection();
+if (!process.env.VSCODE_INSPECTOR_OPTIONS) {
+  _puppeteer.blockResources('stylesheet', 'image', 'media', 'font');
+}
+const _launch = _puppeteer.launch;
+(_puppeteer as any).launch = async function (options?: Puppeteer.LaunchOptions & Puppeteer.BrowserLaunchArgumentOptions & Puppeteer.BrowserConnectOptions) {
+  if (!options) options = {};
+
+  if (options.headless === undefined) {
+    options.headless = process.env.VSCODE_INSPECTOR_OPTIONS ? false : true;
+  }
+
+  options.pipe = true;
+  options.defaultViewport = null;
+
+  if (!process.env.VSCODE_INSPECTOR_OPTIONS) {
+    options.args = [
+      '--ignore-certificate-errors',
+      '--no-sandbox',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--no-default-browser-check',
+      '--no-first-run',
+      '--disable-default-apps',
+      '--disable-popup-blocking',
+      '--disable-translate',
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-renderer-backgrounding',
+      '--disable-device-discovery-notifications',
+      '--disable-extensions-http-throttling'
+    ];
+  } else {
+    options.args = [
+      '--mute-audio'
+    ];
+  }
+
+  const o: Puppeteer.Browser = await _launch.call(this, options);
+
+  return o;
+};
+
+export const puppeteer = _puppeteer;
+
+export function detectNewPage(browser: Puppeteer.Browser, timeout = 30 * 1000) {
+  return new Promise<Puppeteer.Page>((resolve, reject) => {
+    let rejectTimeout: any;
+
+    if (timeout > 0) {
+      rejectTimeout = setTimeout(() => reject(new Error(`Timeout Exceeded: ${timeout}ms exceeded`)), timeout);
+    }
+
+    browser.once('targetcreated', async target => {
+      let newPage: Puppeteer.Page = null as any;
+
+      try {
+        if (target.type() === 'page') {
+          newPage = await target.page();
+          await newPage.bringToFront();
+          const newPagePromise = new Promise<Puppeteer.Page>(resolve2 => newPage.once('domcontentloaded', () => resolve2(newPage)));
+          const isPageLoaded = await newPage.evaluate(() => document.readyState);
+
+          if (!isPageLoaded.match('complete|interactive')) {
+            console.log(`Waiting for DOM content to load on new page`);
+            newPage = await newPagePromise;
+          }
+
+          const htmlLength = await newPage.evaluate(() => document.body.outerHTML.length);
+          console.log(`Found page with ${htmlLength} html length`);
+
+          clearTimeout(rejectTimeout);
+          return resolve(newPage);
+        }
+      } catch (ex: any) {
+        try { await newPage?.close(); } catch (ex: any) { }
+
+        clearTimeout(rejectTimeout);
+        reject(ex);
+      }
+    });
+  });
+}
--- src/update.ts
+++ src/update.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as fs from 'fs';
+import { puppeteer } from './chrome';
 import * as hash from 'object-hash';
 
 // tslint:disable-next-line: no-var-requires
@@ -7,14 +8,22 @@ const cmd = require('node-run-cmd');
 
 const fileLocation = 'src/index.ts';
 
-// https://knowledge.hubspot.com/forms/what-domains-are-blocked-when-using-the-forms-email-domains-to-block-feature
-axios.get('https://f.hubspotusercontent40.net/hubfs/2832391/Marketing/Lead-Capture/free-domains-1.csv')
-  .then(response => {
-    const data = 'export const freeEmailDomains = ' + JSON.stringify(response.data.split(/,\n/g));
+// tslint:disable-next-line: no-floating-promises
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://knowledge.hubspot.com/forms/what-domains-are-blocked-when-using-the-forms-email-domains-to-block-feature');
 
-    if (data.length < 100) {
-      throw new Error('Domain count too low');
-    }
+    const link = await page.evaluate(() => Array.from(document.querySelectorAll<HTMLLinkElement>('article a')).find(x => x.innerText?.trim().toLowerCase() === 'download a csv file')?.href);
+    if (!link) throw new Error('Could not find csv link');
+
+    await browser.close();
+
+    const csvResponse = await axios.get<string>(link);
+    const data = 'export const freeEmailDomains = ' + JSON.stringify(csvResponse.data.split(/[,\n\r]+/g).filter(x => x.length > 0)) + ';';
+
+    if (data.length < 100) throw new Error('Domain count too low');
 
     const curHash = hash(fs.readFileSync(fileLocation).toString());
     const newHash = hash(data);
@@ -23,7 +32,8 @@ axios.get('https://f.hubspotusercontent40.net/hubfs/2832391/Marketing/Lead-Captu
       fs.writeFileSync(fileLocation, data);
       cmd.run('cd dist && npm version --no-git-tag-version patch', { shell: true });
     }
-  })
-  .catch(ex => {
+  }
+  catch (ex) {
     throw ex; // Throw for github action to fail
-  });
+  }
+})();
